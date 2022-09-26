a16b72096ca7370fc994ed9e1e4550cd44da17fb
wlsah0814 <wlsah0814@naver.com>
2022-09-26 12:01:31 +0900

    test commit

---
+++ src/main/java/com/shipnolja/reservation/controller.java
@@ -0,0 +1,14 @@
+package com.shipnolja.reservation;
+
+import lombok.Getter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "test";
+    }
+}
