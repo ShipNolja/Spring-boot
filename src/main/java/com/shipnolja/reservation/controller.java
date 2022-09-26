08412c48b7c0d5161bc020131be9f6235b194aab
wlsah0814 <wlsah0814@naver.com>
2022-09-26 12:39:01 +0900

    react 연동 테스트

--- src/main/java/com/shipnolja/reservation/controller.java
+++ src/main/java/com/shipnolja/reservation/controller.java
@@ -2,12 +2,14 @@ package com.shipnolja.reservation;
 
 import lombok.Getter;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class controller {
 
     @GetMapping("/welcome")
+    @ResponseBody
     public String welcome() {
         return "test123";
     }
