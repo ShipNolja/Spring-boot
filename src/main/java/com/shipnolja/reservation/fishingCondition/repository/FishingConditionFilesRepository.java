02041387b584a14ab47f63b3cbd83cdc980e9aa9
supark0206 <supark0206@gmail.com>
2022-11-15 22:17:41 +0900

    조황정보 등록 완료#18

---
+++ src/main/java/com/shipnolja/reservation/fishingCondition/repository/FishingConditionFilesRepository.java
@@ -0,0 +1,9 @@
+package com.shipnolja.reservation.fishingCondition.repository;
+
+import com.shipnolja.reservation.fishingCondition.model.FishingConditionFiles;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FishingConditionFilesRepository extends JpaRepository<FishingConditionFiles,Long> {
+
+
+}
