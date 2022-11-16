d6f422631ad8ecd91725da44c6867c1149fa4356
wlsah0814 <wlsah0814@naver.com>
2022-11-16 19:22:50 +0900

    회원이 예약한 예약 목록 조회 완료(테스트 완료) #22

--- src/main/java/com/shipnolja/reservation/reservation/repository/ReservationRepository.java
+++ src/main/java/com/shipnolja/reservation/reservation/repository/ReservationRepository.java
@@ -1,14 +1,20 @@
 package com.shipnolja.reservation.reservation.repository;
 
-import com.shipnolja.reservation.fishinginfo.model.FishingInfo;
 import com.shipnolja.reservation.reservation.model.Reservation;
 import com.shipnolja.reservation.user.model.UserInfo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Optional;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     Optional<Reservation> findByReservationDate(LocalDate startDate);
+
+    /* 회원 예약상태에 따른 목록 */
+    Page<Reservation> findByUserInfoAndReservationStatusContaining(UserInfo userInfo, String content, Pageable pageable);
+    
+    /* 회원 예약날짜에 따른 목록 */
+    Page<Reservation> findByUserInfoAndReservationDate(UserInfo userInfo, LocalDate reservationDate, Pageable pageable);
 }
--- src/main/java/com/shipnolja/reservation/user/controller/UserController.java
+++ src/main/java/com/shipnolja/reservation/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.shipnolja.reservation.user.controller;
 
+import com.shipnolja.reservation.reservation.dto.response.ResReservationListDto;
 import com.shipnolja.reservation.ship.dto.request.ShipInfoDto;
 import com.shipnolja.reservation.user.annotation.LoginUser;
 import com.shipnolja.reservation.user.dto.response.ResUserInfoDto;
@@ -14,6 +15,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Api(tags = {"User - ROLE_USER"})
 @RequiredArgsConstructor
@@ -40,6 +43,18 @@ public class UserController {
                                          @ApiParam(value = "선박 이미지", required = true) @RequestPart(value = "image") MultipartFile file){
         Long result = userService.shipRegistration(userInfo,shipInfoDto,file);
         return new ResResultDto(result,"선박 등록을 성공했습니다");
+    }
+
+    /* 회원 예약 목록 조회 (진모)*/
+    @ApiOperation(value = "회원 예약 목록 조회",notes = "회원이 예약한 목록을 조회합니다.")
+    @GetMapping("/reservationList")
+    public List<ResReservationListDto> userReservationList(@LoginUser UserInfo userInfo,
+                                                           @ApiParam(value = "정렬 방법 : asc , desc", required = true) @RequestParam String sortMethod,
+                                                           @ApiParam(value = "검색 기준 : 예약상태, 예약날짜", required = true) @RequestParam String searchBy,
+                                                           @ApiParam(value = "검색값 : 예약완료, 예약취소, 방문완료, xxxx(년)-xx(월)-xx(일)", required = true) @RequestParam String content,
+                                                           int page) {
+
+        return userService.userReservationList(userInfo, page, sortMethod, searchBy, content);
     }
 
 }
